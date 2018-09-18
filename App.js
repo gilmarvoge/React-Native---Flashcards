8e4fd8b916dd3dcd7051ca618ce150bfc21a4410
gilmarvoge <gilmarvoge@gmail.com>
2018-09-18 01:11:22 -0300

    deck

--- App.js
+++ App.js
@@ -1,40 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
+
+import AppStatusBar from './src/components/appStatusBar'
+
 import { purple, white } from './src/utils/colors'
-import { Provider } from 'react-redux'
-import store from './src/store/store'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
-import {MainNavigator} from './src/rotas/rotas'
-
-
-
-function AppStatusBar({ backgroundColor, ...props }) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  )
-}
+import { setLocalNotification } from './src/utils/helpers'
+import MainNavigator from './src/rotas/rotas'
+//import setInitialStorage from './utils/api'
 
 export default class App extends React.Component {
+  
+  componentDidMount() {
+   // setInitialStorage()
+   setLocalNotification()
+
+  }
+
   render() {
     return (
-    
-        <View style={{ flex: 1 }}>
+      
+        <View style={{flex: 1}}>
           <AppStatusBar backgroundColor={purple} barStyle="light-content" />
           <MainNavigator />
         </View>
      
-    );
+    )
   }
 }
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
---
+++ src/views/Deck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Deck extends React.Component {
+ 
+
+  render = () => (
+    <View>
+      <Text>oi</Text>
+      
+    </View>
+  );
+}
